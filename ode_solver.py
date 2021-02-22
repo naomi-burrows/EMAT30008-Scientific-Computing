62bea1fdd6b0c8c12994cd48d44c899b4f3fe7cc
naomi-burrows <73693318+naomi-burrows@users.noreply.github.com>
2021-02-22 15:08:24 +0000

    Updated ode_solver.py to work with systems of ODEs

--- ode_solver.py
+++ ode_solver.py
@@ -2,16 +2,16 @@ import numpy as np
 
 def euler_step(f, x1, t1, h):
     # function does a single Euler step
-    x2 = x1 + h* f(x1,t1)
+    x2 = x1 + h* np.array(f(x1,t1))
     t2 = t1 + h
     return [x2, t2]
 
 def rk4_step(f, x1, t1, h):
     # function does a single RK4 step
-    k1 = f(x1, t1)
-    k2 = f(x1 + h*k1/2, t1 + h/2)
-    k3 = f(x1 + h*k2/2, t1 + h/2)
-    k4 = f(x1 + h*k3, t1 + h)
+    k1 = np.array(f(x1, t1))
+    k2 = np.array(f(x1 + h*k1/2, t1 + h/2))
+    k3 = np.array(f(x1 + h*k2/2, t1 + h/2))
+    k4 = np.array(f(x1 + h*k3, t1 + h))
     x2 = x1 + h*(k1 + 2*k2 + 2*k3 + k4)/6
     t2 = t1 + h
     return [x2, t2]
@@ -30,8 +30,8 @@ def solve_to(step, f, x1, t1, t2, hmax):
 
 def solve_ode(f, x0, t, method, hmax):
     # function returns a series of numerical solution estimates to ode f
-    x = np.zeros_like(t)
-    x[0] = x0[0]
+    x = np.zeros((len(t), len(x0)))
+    x[0] = x0
     for i in range(1, len(t)):
         x[i] = solve_to(method, f, x[i-1], t[i-1], t[i], hmax)
     return x
