66109bb9b9476e51b2f9e016c731b44a61c5f91f
naomi-burrows <73693318+naomi-burrows@users.noreply.github.com>
2021-03-01 22:31:06 +0000

    Added question 2 part 3 to week3.py
    Finding step sizes for each method that give the same error, and timing each method with those step sizes

--- week3.py
+++ week3.py
@@ -1,6 +1,7 @@
 import ode_solver
 import numpy as np
 import matplotlib.pyplot as plt
+import time
 
 # xdot = x
 def f(x,t):
@@ -17,10 +18,10 @@ print(f'Euler approximation (h = {h}) of x(1) = {sol}')
 # Estimate x(1) - RK4
 h = 0.001
 sol = ode_solver.solve_ode(f, x0, t, ode_solver.rk4_step, h)[1][0]
-print(f'RK4   approximation (h = {h}) of x(1) = {sol}')
+print(f'RK4   approximation (h = {h}) of x(1) = {sol}\n')
 
 # Find the Euler and RK4 approximations for x(1) for different values of h
-h = np.logspace(-4, -1, 300)
+h = np.logspace(-5, 0, 300)
 real = np.exp(1)    # real value of x(1)
 sol_euler = [ode_solver.solve_ode(f, x0, t, ode_solver.euler_step, hvalue)[1][0] for hvalue in h]
 sol_rk4 = [ode_solver.solve_ode(f, x0, t, ode_solver.rk4_step, hvalue)[1][0] for hvalue in h]
@@ -36,3 +37,22 @@ plt.ylabel('Absolute Error')
 plt.legend(['Euler', 'RK4'], loc='best')
 plt.savefig('Euler-RK4-Errors.png')
 plt.show()
+
+# Find step sizes for Euler and RK4 that give the same error
+err_to_find = 2.5*10**(-5)
+Euler_h = np.interp(err_to_find, err_euler, h)
+RK4_h = np.interp(err_to_find, err_rk4, h)
+print(f'h values for error of {err_to_find}:')
+print(f'    Euler h = {Euler_h}')
+print(f'    RK4   h = {RK4_h}\n')
+
+# Time each method with the step size that gives error above
+print(f'Time for h values that give error of {err_to_find}:')
+# Euler
+time0 = time.perf_counter()
+ode_solver.solve_ode(f, x0, t, ode_solver.euler_step, Euler_h)
+print(f'    Euler : {time.perf_counter() - time0}s')
+# RK4
+time0 = time.perf_counter()
+ode_solver.solve_ode(f, x0, t, ode_solver.rk4_step, RK4_h)
+print(f'    RK4   : {time.perf_counter() - time0}s')
