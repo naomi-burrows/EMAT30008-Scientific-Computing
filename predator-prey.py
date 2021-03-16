e445bb977928a96b71232a209c3a48de30971057
naomi-burrows <73693318+naomi-burrows@users.noreply.github.com>
2021-03-16 19:13:29 +0000

    Added plot_multiple_ICs function
    plot_multiple_ICs plots phase portraits with multiple initial conditions on the same graph

--- predator-prey.py
+++ predator-prey.py
@@ -6,9 +6,19 @@ def main():
     t = np.linspace(0, 150, 1501)
     b = 0.3
 
-    plot_time_series(t, b)
+    # plot_time_series(t, b)
 
-    plot_phase_portrait(t, b)
+    # plot_phase_portrait(t, b)
+
+    plot_multiple_ICs(t, b, ((0.1, 0),
+                             (0.1, 0.2),
+                             (0.1, 0.5),
+                             (0.3, 0),
+                             (0.3, 0.2),
+                             (0.3, 0.5),
+                             (0.5, 0),
+                             (0.5, 0.2),
+                             (0.5, 0.5)))
 
 def predator_prey(u, t, b):
     a = 1
@@ -41,6 +51,22 @@ def plot_phase_portrait(t, b):
     plt.plot(x, y)
     plt.xlabel('x')
     plt.ylabel('y')
+    plt.show()
+
+def plot_multiple_ICs(t, b, u0s):
+    legend_list = []
+    for u0 in u0s:
+        sol = ode_solver.solve_ode(lambda u, t: predator_prey(u, t, b), u0, t, ode_solver.rk4_step, 0.001)
+
+        x = sol[:, 0]
+        y = sol[:, 1]
+
+        plt.plot(x, y)
+        legend_list.append("(x0, y0) = " + str(u0))
+
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.legend(legend_list)
     plt.show()
 
 if __name__ == "__main__":
