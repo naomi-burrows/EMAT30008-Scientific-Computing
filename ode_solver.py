0797dc6db7d1f9059ea625e47c4416f26a1cef88
naomi-burrows <73693318+naomi-burrows@users.noreply.github.com>
2021-03-22 17:01:45 +0000

    Added docstrings to ode_solver.py
    Also added spacing to make the code easier to read.
    
    Changed num_steps in solve_to from int((t2 - t1) / hmax) to math.floor((t2 - t1) / hmax) to make code more robust.

--- ode_solver.py
+++ ode_solver.py
@@ -1,24 +1,63 @@
 import numpy as np
+import math
 
 def euler_step(f, x1, t1, h):
-    # function does a single Euler step
+    """
+    Does a single Euler step. Returns [x_{n+1}, t_{n+1}].
+
+        Parameters:
+            f (function):   function to do the euler step on
+            x1:             x_n - initial value(s) for x
+            t1 (float):     t_n - inital time
+            h (float):      stepsize
+
+        Returns:
+            [x_{n+1}, t_{n+1}] = [x at t + h, t + h]
+    """
     x2 = x1 + h* np.array(f(x1,t1))
     t2 = t1 + h
+
     return [x2, t2]
 
 def rk4_step(f, x1, t1, h):
-    # function does a single RK4 step
+    """
+    Does a single RK4 step. Returns [x_{n+1}, t_{n+1}].
+
+        Parameters:
+            f (function):   function to do the RK4 step on
+            x1:             x_n - initial value(s) for x
+            t1 (float):     t_n - inital time
+            h (float):      stepsize
+
+        Returns:
+            [x_{n+1}, t_{n+1}] = [x at t + h, t + h]
+    """
     k1 = np.array(f(x1, t1))
     k2 = np.array(f(x1 + h*k1/2, t1 + h/2))
     k3 = np.array(f(x1 + h*k2/2, t1 + h/2))
     k4 = np.array(f(x1 + h*k3, t1 + h))
+
     x2 = x1 + h*(k1 + 2*k2 + 2*k3 + k4)/6
     t2 = t1 + h
+
     return [x2, t2]
 
 def solve_to(step, f, x1, t1, t2, hmax):
-    # function solves from x1,t1 to x2,t2 in steps no bigger than hmax
-    num_steps = int((t2 - t1) / hmax)
+    """
+    Solves from x1,t1 to x2,t2 in steps no bigger than hmax. Returns x value at time t2.
+
+        Parameters:
+            step (function):    type of step to do (euler_step or rk4_step)
+            f (function):       function to solve
+            x1:                 x_n - initial value(s) for x
+            t1 (float):         t_n - inital time
+            t2 (float):         t_{n+1} - time to solve to
+            hmax (float):       maximum stepsize
+
+        Returns:
+            solution(s) for x at time t2
+    """
+    num_steps = math.floor((t2 - t1) / hmax)
     x = x1
     t = t1
     for i in range(num_steps):
@@ -29,7 +68,19 @@ def solve_to(step, f, x1, t1, t2, hmax):
     return x
 
 def solve_ode(f, x0, t, method, hmax):
-    # function returns a series of numerical solution estimates to ode f
+    """
+    Returns a series of numerical solution estimates to ode f.
+
+        Parameters:
+            f (function):       function to solve
+            x0:                 initial value(s) for x
+            t (ndarray):        time values to approximate x for
+            method (function):  method by which to approximate solution (euler_step or rk4_step)
+            hmax (float):       maximum stepsize
+
+        Returns:
+            solutions for x at each time value in t
+    """
     x = np.zeros((len(t), len(x0)))
     x[0] = x0
     for i in range(1, len(t)):
