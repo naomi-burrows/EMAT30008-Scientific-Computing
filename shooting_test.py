a960c1f0d6c3c8e914d75829f9331e6fdf6c2226
naomi-burrows <73693318+naomi-burrows@users.noreply.github.com>
2021-04-05 18:42:45 +0100

    Added analytic solution to Hopf bifurcation
    Added the function and plotted its time series and phase portrait.
    
    Also fixed a typo in the docstring for hopf.

--- shooting_test.py
+++ shooting_test.py
@@ -1,10 +1,11 @@
 import numpy as np
+import matplotlib.pyplot as plt
 import simulate_ode
 import shooting
 
 def hopf(u, t, beta, sigma=-1.0):
     """
-    ODE for the Hopf bifurcation normal form. Deturns du1/dt and du2/dt
+    ODE for the Hopf bifurcation normal form. Returns du1/dt and du2/dt
 
         Parameters:
             u (tuple):      vector in the form (u1, u2)
@@ -21,6 +22,22 @@ def hopf(u, t, beta, sigma=-1.0):
     du2dt = u1 + beta*u2 + sigma*u2*(u1**2 + u2**2)
 
     return np.array((du1dt, du2dt))
+
+def real_sol(t, beta, theta=0.0):
+    """
+    Analytic solution of the Hopf bifurcation normal form. Returns u1 and u2
+
+        Parameters:
+            t (float):      value for time
+            beta (float):   parameter in equations
+            theta (float):  default=0. Phase
+
+        Returns:
+            numpy array of (u1, u2)
+    """
+    u1 = np.sqrt(beta) * np.cos(t + theta)
+    u2 = np.sqrt(beta) * np.sin(t + theta)
+    return np.array((u1, u2))
 
 t = np.linspace(0, 40, 401)
 
@@ -41,3 +58,15 @@ t = np.linspace(0, T, 101)
 simulate_ode.plot_time_series(lambda u,t: hopf(u, t, beta=1), u0, t, labels=['u1', 'u2'])
 # Plot limit cycle as phase portrait
 simulate_ode.plot_phase_portrait(lambda u,t: hopf(u, t, beta=1), u0, t, labels=['u1', 'u2'])
+
+# Plot real solution - time series
+u1, u2 = real_sol(t, beta=1)
+plt.plot(t, u1, t, u2)
+plt.xlabel('time, t')
+plt.legend(('u1', 'u2'))
+plt.show()
+# Plot real solution - phase portrait
+plt.plot(u1, u2)
+plt.xlabel('u1')
+plt.ylabel('u2')
+plt.show()
